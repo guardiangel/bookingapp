6e5ca52640ad57ca581c2bf05faed0394b69d55f
guardiangel <felix.sgq@gmail.com>
2023-02-10 21:23:23 -0500

    Hotel CRUD

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -16,11 +16,50 @@ router.post("/", async (req, resp) => {
 });
 
 //update
+router.put("/:id", async (req, resp) => {
+  try {
+    //Hotel.findByIdAndUpdate will return the one before updating.
+    //need to use {new:true} if we want to return the updated one.
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    resp.status(200).json(updateHotel);
+  } catch (error) {
+    resp.status(500).json(error);
+  }
+});
 
 //delete
+router.delete("/:id", async (req, resp) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    resp.status(200).json("Hotel has been deleted");
+  } catch (error) {
+    resp.status(500).json(error);
+  }
+});
 
 //get
-
+router.get("/:id", async (req, resp) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    resp.status(200).json(hotel);
+  } catch (error) {
+    resp.status(500).json(error);
+  }
+});
 //get all
-
+router.get("/", async (req, resp) => {
+  try {
+    const hotels = await Hotel.find();
+    resp.status(200).json(hotels);
+  } catch (error) {
+    resp.status(500).json(error);
+  }
+});
 export default router;
