3ca50c992982e2eb4434c537f8d445b125ea34ac
guardiangel <felix.sgq@gmail.com>
2023-02-12 22:08:52 -0500

    login and eliminate some attributes

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import { createError } from "../utils/error.js";
 
 export const register = async (req, resp, next) => {
   try {
@@ -13,6 +14,27 @@ export const register = async (req, resp, next) => {
     });
     const saveUser = await newUser.save();
     resp.status(200).json(saveUser);
+  } catch (error) {
+    next(error);
+  }
+};
+export const login = async (req, resp, next) => {
+  try {
+    const user = await User.findOne({ userName: req.body.userName });
+    if (!user) {
+      return next(createError(404, "user not found!"));
+    }
+    const passwordCorrectFlag = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!passwordCorrectFlag) {
+      return next(createError(400, "wrong password or username"));
+    }
+    //Using _doc in this way can eliminate the password and isAdmin
+    const { password, isAdmin, ...others } = user._doc;
+
+    resp.status(200).json({ ...others });
   } catch (error) {
     next(error);
   }
