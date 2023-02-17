3a8853ce2e2d0ba4548b2abe0fb33727077a41fe
guardiangel <felix.sgq@gmail.com>
2023-02-16 22:41:52 -0500

    User login

---
+++ src/login/Login.js
@@ -0,0 +1,59 @@
+import { useContext, useState } from "react";
+import "./Login.css";
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { user, loading, error, dispatch } = useContext(AuthContext);
+
+  //redirect to another page after successfully login
+  const navigate = useNavigate();
+
+  const handleChange = async (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", credentials);
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+      navigate("/");
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data });
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="userName"
+          id="userName"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <button disabled={loading} onClick={handleClick} className="lButton">
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
