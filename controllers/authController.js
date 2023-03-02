4ad62aa4c249dd28f6775f4176f82481fbca53c1
guardiangel <felix.sgq@gmail.com>
2023-03-01 21:16:51 -0500

    Admin user

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,10 +8,11 @@ export const register = async (req, resp, next) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
-      userName: req.body.userName,
-      email: req.body.email,
+      // userName: req.body.userName,
+      // email: req.body.email,
+      ...req.body,
       password: hash,
-      isAdmin: req.body.isAdmin,
+      // isAdmin: req.body.isAdmin,
     });
     const saveUser = await newUser.save();
     resp.status(200).json(saveUser);
